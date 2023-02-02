563609cf4f04c31f268469c5b8ed006f066d8304
Ray Hung <howard88tw@gmail.com>
2023-02-01 23:14:40 -0500

    - Add more fields in visual summary detail page. - Add filter buttons on visual summary listing page

--- lib/visual_summary/visual_summary_detail_page.dart
+++ lib/visual_summary/visual_summary_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 import 'package:emoroid_digest_app/models/visual_summary.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class VisualSummaryDetailPage extends StatefulWidget {
   const VisualSummaryDetailPage(
@@ -20,11 +21,47 @@ class VisualSummaryDetailPage extends StatefulWidget {
 
 class _VisualSummaryDetailPageState extends State<VisualSummaryDetailPage> {
   final double iconSize = 30;
+  final double fieldFontSize = 16;
+
+  Widget detailField(String title, String value) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+                child: Text(
+              title,
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontSize: fieldFontSize,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+          ],
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                value,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: fieldFontSize,
+                ),
+              ),
+            )
+          ],
+        ),
+        const Divider(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool isJpegOrPng =
-        widget.visualSummary.linkVisualSummarySource.endsWith('jpg') ||
-            widget.visualSummary.linkVisualSummarySource.endsWith('png');
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
@@ -44,13 +81,13 @@ class _VisualSummaryDetailPageState extends State<VisualSummaryDetailPage> {
               textAlign: TextAlign.center,
               style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-            if (isJpegOrPng)
-              Image.network(widget.visualSummary.linkVisualSummarySource)
-            else
+            if (widget.visualSummary.mimeTypeVisualSummary == "application/pdf")
               SizedBox(
                   height: 240,
                   child: SfPdfViewer.network(
-                      widget.visualSummary.linkVisualSummarySource)),
+                      widget.visualSummary.linkVisualSummarySource))
+            else
+              Image.network(widget.visualSummary.linkVisualSummarySource),
             Padding(
                 padding: const EdgeInsets.only(left: 8, right: 8),
                 child: Column(
@@ -76,111 +113,49 @@ class _VisualSummaryDetailPageState extends State<VisualSummaryDetailPage> {
                         ),
                         const SizedBox(width: 10),
                         IconButton(
+                          onPressed: () async {
+                            final uri = Uri.parse(
+                                widget.visualSummary.linkOriginalManuscript);
+                            if (await canLaunchUrl(uri)) {
+                              await launchUrl(uri);
+                            } else {
+                              throw 'Could not launch ${widget.visualSummary.linkOriginalManuscript}';
+                            }
+                          },
+                          icon: const Icon(Icons.description_outlined),
+                          iconSize: iconSize,
+                        ),
+                        const SizedBox(width: 10),
+                        IconButton(
                           onPressed: () {},
                           icon: const Icon(Icons.share_outlined),
                           iconSize: iconSize,
                         )
                       ],
                     ),
-                    Row(
-                      children: const [
-                        Text(
-                          "Fellow Author",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                        ),
-                      ],
-                    ),
                     const SizedBox(
                       height: 8,
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          widget.visualSummary.fellowAuthor,
-                          textAlign: TextAlign.start,
-                          style: const TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Divider(),
-                    Row(
-                      children: const [
-                        Text(
-                          "Year Guideline Published",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "${widget.visualSummary.yearGuidelinePublished}",
-                          textAlign: TextAlign.start,
-                          style: const TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Divider(),
-                    Row(
-                      children: const [
-                        Text(
-                          "Society",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          widget.visualSummary.giSocietyJournal,
-                          textAlign: TextAlign.start,
-                          style: const TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Divider(),
-                    Row(
-                      children: const [
-                        Text(
-                          "Keywords",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      widget.visualSummary.keywords.join(", "),
-                      textAlign: TextAlign.start,
-                      style: const TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
+                    detailField("Year Guideline Published",
+                        "${widget.visualSummary.yearGuidelinePublished}"),
+                    detailField(
+                        "Society", widget.visualSummary.giSocietyJournal),
+                    detailField("Organ Systems",
+                        widget.visualSummary.organSystems.join(", ")),
+                    detailField(
+                        "Keywords", widget.visualSummary.keywords.join(", ")),
+                    detailField(
+                        "Guideline Authors (First two and last listed author)",
+                        widget.visualSummary.guidelineAuthors.join(", ")),
+                    detailField(
+                        "Recorded Podcast",
+                        widget.visualSummary.recordedPodcastTitle == ""
+                            ? "N/A"
+                            : widget.visualSummary.recordedPodcastTitle),
+                    detailField("Visual Summary Fellow Author",
+                        widget.visualSummary.fellowAuthor),
+                    detailField("Visual Summary Release Date",
+                        "${widget.visualSummary.dateReleased.toDate().year.toString()}-${widget.visualSummary.dateReleased.toDate().month.toString().padLeft(2, '0')}-${widget.visualSummary.dateReleased.toDate().day.toString().padLeft(2, '0')}"),
                   ],
                 )),
           ],
