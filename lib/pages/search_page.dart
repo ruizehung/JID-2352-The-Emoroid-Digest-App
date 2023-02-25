e8e7603f5da30befd5a1591f36edac66758b5079
Tuan Nguyen <maytinhxachtay@Tuans-MacBook-Air.local>
2023-02-25 02:20:38 -0500

    list the keyword

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,8 +1,11 @@
+import 'package:emoroid_digest_app/utils/isar_service.dart';
 import 'package:flutter/material.dart';
+import '../../models/visual_summary.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
-
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
@@ -12,11 +15,25 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text("This is search page"),
         ElevatedButton(
           onPressed: () => Navigator.of(context).pop(),
           child: const Text("Go back"),
         ),
+        ElevatedButton(
+          onPressed: () {
+            print(IsarService().getUniqueKeywords());
+          },
+          child: const Text("Go back"),
+        ),
+        TextField(
+            onChanged: (value) {},
+            controller: null,
+            decoration: const InputDecoration(
+                labelText: "Search",
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25))))),
+        const Text("This is search page"),
       ],
     );
   }
