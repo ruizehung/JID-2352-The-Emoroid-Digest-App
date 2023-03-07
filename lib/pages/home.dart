cc5b7b855179a384d85b91d999a59c53c1fdad7f
Leonard Thong <leonard@lawn-143-215-117-88.lawn.gatech.edu>
2023-03-07 18:26:44 -0500

    Add latest podcast to homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,11 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:emoroid_digest_app/pages/podcast/podcast_card.dart';
 import 'package:emoroid_digest_app/pages/visual_summary/visual_summary_card.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/firebase.dart';
 import '../utils/isar_service.dart';
+import '../models/podcast.dart';
 import '../models/visual_summary.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +18,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool isLoading = false;
   VisualSummary? visualSummarySelected;
+  Podcast? podcastSelected;
 
   @override
   void initState() {
@@ -41,6 +44,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void setPodcastSelected(Podcast? p) {
+    setState(() {
+      podcastSelected = p;
+    });
+  }
+
   Future<VisualSummary> _getLatestVisualSummary() async {
     List<VisualSummary> list = await IsarService().getVisualSummariesWithThumbnail();
     VisualSummary vs = list[0];
@@ -50,6 +59,17 @@ class _HomePageState extends State<HomePage> {
       }
     }
     return vs;
+  }
+
+  Future<Podcast> _getLatestPodcast() async {
+    List<Podcast> list = await IsarService().getPodcasts();
+    Podcast p = list[0];
+    for (int i = 1; i < list.length; i++) {
+      if (list[i].dateReleased.compareTo(p.dateReleased) > 0) {
+        p = list[i];
+      }
+    }
+    return p;
   }
 
   @override
@@ -79,6 +99,30 @@ class _HomePageState extends State<HomePage> {
                 ));
               } else {
                 return VisualSummaryCard(visualSummary: future.data!);
+              }
+            }),
+        const Padding(
+          padding: EdgeInsets.only(left: 25.0, top: 20),
+          child: Text(
+            'Latest Podcast',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        FutureBuilder<Podcast>(
+            future: _getLatestPodcast(),
+            builder: (BuildContext context, AsyncSnapshot<Podcast> future) {
+              if (isLoading || !future.hasData) {
+                return const Center(
+                    child: Padding(
+                  padding: EdgeInsets.only(top: 60.0),
+                  child: CircularProgressIndicator(),
+                ));
+              } else {
+                return PodcastCard(podcast: future.data!);
               }
             }),
       ],
