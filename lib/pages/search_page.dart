4d9c29f509c354469ba92e7842e2e480db879b77
Tuan <npatuan1011@gmail.com>
2023-03-08 15:21:27 -0500

    work for both vs and podcast without search

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,3 +1,7 @@
+import 'package:emoroid_digest_app/models/master.dart';
+import 'package:emoroid_digest_app/models/podcast.dart';
+import 'package:emoroid_digest_app/pages/podcast/podcast_card.dart';
+import 'package:emoroid_digest_app/pages/podcast/podcasts_list_page.dart';
 import 'package:emoroid_digest_app/pages/visual_summary/visual_summary_card.dart';
 import 'package:emoroid_digest_app/utils/isar_service.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +18,10 @@ class _SearchPageState extends State<SearchPage> {
   String search = ""; //for searching
   final searchList = <String>{};
   final TextEditingController _searchController = TextEditingController();
-
-  Future<List<VisualSummary>> result = IsarService().getVisualSummariesWithThumbnail();
+  Future<List<Master>> result = IsarService().getMasterList();
 
   bool isSearch = false;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -43,7 +47,11 @@ class _SearchPageState extends State<SearchPage> {
                   child: TextField(
                       onChanged: (value) {
                         setState(() {
-                          result = IsarService().getVisualSummariesResultAfterSearch(value);
+                          if (value == "") {
+                            print(value);
+                            result = IsarService().getMasterList();
+                          }
+                          result = _initialList();
                         });
                       },
                       controller: _searchController,
@@ -58,9 +66,9 @@ class _SearchPageState extends State<SearchPage> {
             )),
         Flexible(
             fit: FlexFit.loose,
-            child: FutureBuilder<List<VisualSummary>>(
+            child: FutureBuilder<List<Master>>(
                 future: result,
-                builder: (BuildContext context, AsyncSnapshot<List<VisualSummary>> future) {
+                builder: (BuildContext context, AsyncSnapshot<List<Master>> future) {
                   var data = future.data;
                   if (data == null) {
                     return const Center(child: CircularProgressIndicator());
@@ -75,15 +83,65 @@ class _SearchPageState extends State<SearchPage> {
                         scrollDirection: Axis.vertical,
                         cacheExtent: 999,
                         itemCount: future.data!.length,
-                        itemBuilder: (context, index) => VisualSummaryCard(
-                          visualSummary: future.data![index],
-                        ),
+                        itemBuilder: (context, index) => (future.data![index] is VisualSummary)
+                            ? VisualSummaryCard(visualSummary: future.data![index] as VisualSummary)
+                            : PodcastCard(podcast: future.data![index] as Podcast),
+                        //   print("ss");
+                        //   if (future.data![index] is VisualSummary) {
+                        //     print(future.data![index]);
+                        //     VisualSummaryCard(
+                        //       visualSummary: future.data![index] as VisualSummary,
+                        //     );
+                        //   } else {
+                        //     print("pod");
+                        //     PodcastCard(
+                        //       podcast: future.data![index] as Podcast,
+                        //     );
+                        //   }
+                        // }
                       );
                     }
                   }
+
                   ;
                 })),
       ],
     );
+  }
+
+//   Future<List<Master>> combineLists() async{
+//     int a = 01;
+// return a;
+//     //print(a);
+//   }
+
+  Future<List<Master>> _initialList() async {
+    Set<Master> msSet = Set();
+
+    Future<List<VisualSummary>> s = IsarService().getVisualSummariesWithThumbnail();
+    await s.then((value) {
+      for (var vs in value) {
+        msSet.add(vs);
+      }
+    });
+    Future<List<Podcast>> p = IsarService().getPodcasts();
+    await p.then((value) {
+      for (var pc in value) {
+        msSet.add(pc);
+      }
+    });
+    //  print(vs.length);
+    //print(pc.length);
+    List<Master> ms = msSet.toList();
+
+    return ms;
+  }
+
+  void assd() {
+    VisualSummary f = VisualSummary();
+    Set<VisualSummary> a = Set();
+    a.add(f);
+    Future<List<Master>> result = IsarService().getVisualSummariesWithThumbnail();
+    result.then((value) => (f = value[1] as VisualSummary));
   }
 }
