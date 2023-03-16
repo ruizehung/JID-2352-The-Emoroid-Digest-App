20afb3d1496bcda56782161e8da88cf8006ad299
Ray Hung <howard88tw@gmail.com>
2023-03-15 21:38:28 -0400

    Add privacy page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:emoroid_digest_app/pages/bottom_nav_bar_state.dart';
+import 'package:emoroid_digest_app/pages/drawer.dart';
 import 'package:emoroid_digest_app/pages/home.dart';
 import 'package:emoroid_digest_app/pages/notification_page.dart';
 import 'package:emoroid_digest_app/pages/podcast/podcast_bar.dart';
@@ -320,29 +321,8 @@ class _TheEmoroidDigestAppState extends State<TheEmoroidDigestApp> with WidgetsB
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    final TextStyle textStyle = theme.textTheme.bodyMedium!;
     final bottomNavBarState = Provider.of<BottomNavBarState>(context);
     bottomNavBarState.navigatorKey = navigatorKey;
-    final List<Widget> aboutBoxChildren = <Widget>[
-      const SizedBox(height: 24),
-      RichText(
-        text: TextSpan(
-          children: <TextSpan>[
-            TextSpan(
-                style: textStyle,
-                text: "The Emoroid Digest App is a mobile app available on both iOS and Android platforms that allow"
-                    "users to access the visual summaries and podcasts released by The Emoroid Digest."
-                    "Users are able to view and download visual summaries and listen to podcasts. See "),
-            TextSpan(
-                style: textStyle.copyWith(color: theme.colorScheme.primary),
-                text:
-                    'https://med.emory.edu/departments/medicine/divisions/digestive-diseases/education/emoroid-digest.html'),
-            TextSpan(style: textStyle, text: ' for more info.'),
-          ],
-        ),
-      ),
-    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -411,45 +391,7 @@ class _TheEmoroidDigestAppState extends State<TheEmoroidDigestApp> with WidgetsB
           _onNavButtonTapped(val, bottomNavBarState);
         },
       ),
-      drawer: Drawer(
-        child: Column(children: [
-          Container(
-              padding: const EdgeInsets.only(top: 80),
-              child: ListTile(
-                leading: const Icon(
-                  Icons.question_mark,
-                  size: 20,
-                ),
-                title: const Text(
-                  "About",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                onTap: () async {
-                  Navigator.of(context).pop();
-                  showAboutDialog(
-                    context: context,
-                    applicationIcon: Image.asset(
-                      "assets/logo.png",
-                      width: 50,
-                      height: 50,
-                    ),
-                    applicationName: 'Emoroid Digest App',
-                    applicationVersion: 'April 2023',
-                    applicationLegalese: '\u{a9} 2023 The Emoroid Digest Team',
-                    children: aboutBoxChildren,
-                  );
-                },
-              )
-
-              // TextButton(
-              //   onPressed: () async {},
-              //   child: const Text("About"),
-              // ),
-              )
-        ]),
-      ),
+      drawer: const HomePageDrawer(),
     );
   }
 }
