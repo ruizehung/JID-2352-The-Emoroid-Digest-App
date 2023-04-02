97b847533ff1f8627bdc056e7acbe361c6ccd824
Tuan <npatuan1011@gmail.com>
2023-04-02 01:49:57 -0400

    json key working

---
+++ lib/models/feedback_model.dart
@@ -0,0 +1,9 @@
+class FeedbackModel {
+  static final String time = 'time';
+  static final String name = 'name';
+  static final String email = "Email";
+  static final String medical = 'medical';
+  static final String detail = 'detail';
+
+  static List<String> getFields() => [time, name, medical, detail];
+}
